cc44e7c0717e0225e6746f177d676d12197ba57a
Belinda Shi <beeshi@mit.edu>
2018-07-11 10:52:59 -0400

    Add script for making model permutations

---
+++ make_models.py
@@ -0,0 +1,34 @@
+import itertools
+import argparse
+import json
+settings = {
+    "epsilon_frames": [2000000],
+     "epsilon_start": [2.0],
+     "epsilon_final": [0.1],
+     "learning_rate": [0.00006, 0.00001],
+             "gamma": [0.99],
+         "dqn_model": ["FSADQNAppendToFC", "FSADQN"],
+               "fsa": [True],
+           "machine": ["ngcv4"],
+    "replay_initial": [500],
+    "video_interval": [1000],
+        "frame_stop": [3000]
+}
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reps", default=1, type=int, help="Number of repeat jobs")
+    parser.add_argument("--file", default='', help="Output file")
+
+    args = parser.parse_args()
+    if len(args.file) == 0:
+        args.file = "jobs.json"
+
+    # Makes all the permutations of the above settings and writes them to json file
+    keys, values = zip(*settings.items())
+    jobs = [dict(zip(keys, v)) for v in itertools.product(*values)]
+    print(len(jobs), "jobs created")
+    print("Writing each job", args.reps, "time(s) to", args.file)
+
+    with open(args.file, "w") as f:
+        f.write(json.dumps(jobs))
